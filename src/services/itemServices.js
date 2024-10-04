5a2cceb354b566f92f3ed16b285d28ae316acfb4
danny8823 <dannyyoo714@gmail.com>
2024-10-04 11:23:44 -0700

    url configured

--- src/services/itemServices.js
+++ src/services/itemServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-
+import { BASE_URL } from '../utils/url'
 export const listItemsApi = async(category)=> {
-    const response = await axios.get('http://localhost:3333/api/items/list',{
+    const response = await axios.get(`${BASE_URL}/items/list`,{
         params: {
             category,
         }
@@ -11,6 +11,6 @@ export const listItemsApi = async(category)=> {
 
 
 export const listSingleItemApi = async(id) => {
-    const response = await axios.get(`http://localhost:3333/api/items/list/${id}`)
+    const response = await axios.get(`${BASE_URL}/list/${id}`)
     return response.data
 }
